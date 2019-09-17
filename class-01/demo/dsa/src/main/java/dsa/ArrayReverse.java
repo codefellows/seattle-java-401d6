7e0aee1832718114cd4a585c6167a119e678e6f6
Michelle Ferreirae <mnfmnfm@gmail.com>
2019-09-16 17:04:23 -0700

    add dsa demo

---
+++ class-01/demo/dsa/src/main/java/dsa/ArrayReverse.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArrayReverse {
+    // main method for running my code
+    public static void main(String[] args) {
+        int[] arrToReverse = new int[]{2,6,9,12};
+        int[] reversedArray = reverseArray(arrToReverse);
+        System.out.println(Arrays.toString(reversedArray));
+    }
+
+    // arrayReverse method
+
+    public static int[] reverseArray(int[] arr) {
+        return arr;
+    }
+}
